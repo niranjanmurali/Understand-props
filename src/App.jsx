65d4704420f044eacc9452758dfe2cfe909398ad
niranjanmurali <godofgenjudsu@email.com>
2024-04-04 10:59:23 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+import React from 'react';
 import './App.css';
 import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
 
-function App() {
-  // code here
+function App(props) {
+  let imgArray = props.imageData()
+  return(
+      <>
+        <h1 className='h1'>Kalvium Gallery</h1>
+          <div className="grid">
+            {/* <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" /> */}
+            {
+              imgArray.map(elem =>{
+                return(
+                  <img src={elem.img}/>
+                )
+              })
+            }
+          </div>
+      </>
+
+  )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -1,30 +1,30 @@
-import { Component } from "react";
-import "./App.css"
+import React from "react";
+// import "./App.css" 
 import elephant from "./images/elephant.jpeg";
 
-export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
+export default class AppClass extends React.Component{
 
-  // code here
+
+  render(){
+    let imgArray = this.props.imageData()
+    return(
+      <>
+        <h1 className='h1'>Kalvium Gallery</h1>
+          <div className="grid">
+            {/* <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" />
+            <img src={imgArray[0].img} alt="" /> */}
+            {
+              imgArray.map(elem =>{
+                return(
+
+                  <img src={elem.img}/>
+                )
+              })
+            }
+          </div>
+      </>
+    )
+  }
 }
